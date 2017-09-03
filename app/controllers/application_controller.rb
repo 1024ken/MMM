42ab599cabce922a547f7e2c90187975b65d75f7
渡辺健一 <watanabe-kenichi@rouge.plala.or.jp>
2017-09-03 15:11:54 +0900

    devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # before_action :check_logined
 
-  # private
-  # def check_logined
-  # end
+  private
+  def check_logined
+  end
 end
