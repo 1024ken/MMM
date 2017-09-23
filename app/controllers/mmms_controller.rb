505a67541a1005ded2bb1fef3125dc035514dd6c
渡辺健一 <watanabe-kenichi@rouge.plala.or.jp>
2017-09-23 15:29:27 +0900

    blog name

--- app/controllers/mmms_controller.rb
+++ app/controllers/mmms_controller.rb
@@ -2,10 +2,6 @@ class MmmsController < InheritedResources::Base
   before_action :authenticate_user!
 
   def index
-    # @mmms = mmm.all
-    # case @mmms
-    # when "spring" then
-    # link_to edit_mmm_path
     redirect_to new_user_session_path unless current_user
   end
   private
